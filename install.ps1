ab9f9eca9f4baf556c5d7d7475d5c99ae585ffab
Aroudight-mo <78698190+Aroudight-mo@users.noreply.github.com>
2025-05-23 11:47:40 +0800

    Update install.ps1
    增加paddlepaddle2.3.2拉取地址

--- install.ps1
+++ install.ps1
@@ -30,8 +30,9 @@ if (!(Test-Path -Path "venv")) {
 Check "activate venv failed"
 
 pip install pyelftools==0.29 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install paddlepaddle==2.3.2 -f https://www.paddlepaddle.org.cn/whl/windows/mkl/avx/stable.html
 pip install pdf2docx==0.5.6 -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install --upgrade -r requirements.txt -i https://mirror.baidu.com/pypi/simple
+pip install --upgrade -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
 
 Write-Output "install complete"
 Read-Host | Out-Null ;
